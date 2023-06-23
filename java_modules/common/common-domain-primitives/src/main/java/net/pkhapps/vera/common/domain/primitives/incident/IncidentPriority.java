b123130157c57898282b928afbc6a10e0aaedad2
Petter Holmström <petter.holmstrom@outlook.com>
2023-06-23 19:59:12 +0300

    Add IncidentPriority

---
+++ java_modules/common/common-domain-primitives/src/main/java/net/pkhapps/vera/common/domain/primitives/incident/IncidentPriority.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 Petter Holmström
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.pkhapps.vera.common.domain.primitives.incident;
+
+/**
+ * Enumeration of incident priorities.
+ */
+public enum IncidentPriority {
+    /**
+     * Highest priority
+     */
+    A,
+    /**
+     * Second-highest priority.
+     */
+    B,
+    /**
+     * Second-lowest priority.
+     */
+    C,
+    /**
+     * Lowest priority.
+     */
+    D,
+    /**
+     * No priority or priority unknown.
+     */
+    N
+}
