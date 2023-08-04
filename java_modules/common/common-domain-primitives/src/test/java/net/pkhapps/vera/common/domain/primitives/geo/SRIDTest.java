8139400b57302b367a4194fba303c90b55604423
Petter Holmström <petter.holmstrom@outlook.com>
2023-08-04 20:26:14 +0300

    Add SRIDTest.java

--- java_modules/common/common-domain-primitives/src/test/java/net/pkhapps/vera/common/domain/primitives/geo/SRIDTest.java
+++ java_modules/common/common-domain-primitives/src/test/java/net/pkhapps/vera/common/domain/primitives/geo/SRIDTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Petter Holmström
+ * Copyright (c) 2023 Petter Holmström
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,41 +13,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.pkhapps.vera.domain.primitives.geo;
+package net.pkhapps.vera.common.domain.primitives.geo;
+
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for {@link SRID}.
- *
- * @author Petter Holmström
- */
 public class SRIDTest {
 
     @Test
-    public void srids_cannot_be_below_minimum() {
+    void srids_cannot_be_below_minimum() {
         assertThatThrownBy(() -> new SRID(SRID.MIN - 1)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void srids_cannot_be_above_maximum() {
+    void srids_cannot_be_above_maximum() {
         assertThatThrownBy(() -> new SRID(SRID.MAX + 1)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void minimum_srid_can_be_created() {
+    void minimum_srid_can_be_created() {
         assertThat(new SRID(SRID.MIN).value()).isEqualTo(SRID.MIN);
     }
 
     @Test
-    public void maximum_srid_can_be_created() {
+    void maximum_srid_can_be_created() {
         assertThat(new SRID(SRID.MAX).value()).isEqualTo(SRID.MAX);
     }
 
     @Test
-    public void srids_with_same_value_are_considered_equal() {
+    void srids_with_same_value_are_considered_equal() {
         var srid1 = new SRID(4326);
         var srid2 = new SRID(4326);
         assertThat(srid1).isEqualTo(srid2);
@@ -55,10 +51,16 @@ public class SRIDTest {
     }
 
     @Test
-    public void srids_with_different_values_are_not_considered_equal() {
+    void srids_with_different_values_are_not_considered_equal() {
         var srid1 = new SRID(4326);
         var srid2 = new SRID(26717);
         assertThat(srid1).isNotEqualTo(srid2);
         assertThat(srid1.hashCode()).isNotEqualTo(srid2.hashCode());
+    }
+
+    @Test
+    void toString_has_been_overridden() {
+        var srid = new SRID(4326);
+        assertThat(srid.toString()).isEqualTo("SRID{4326}");
     }
 }
