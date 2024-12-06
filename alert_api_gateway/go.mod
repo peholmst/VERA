f9bcd1783f45bf710b8e31599aafdee26771b29c
Petter Holmström <petter.holmstrom@outlook.com>
2024-12-06 14:18:24 +0200

    Clean up project

--- alert_api_gateway/go.mod
+++
@@ -1,3 +0,0 @@
-module vera.pkhapps.net/alert_api_gateway
-
-go 1.19
--- ts_modules/vera-dispatcher/src/api/i18n.ts
+++
@@ -1,5 +0,0 @@
-export type Locale = "fi_FI" | "sv_FI" | "smn_FI" | "sme_FI" | "sms_FI";
-
-export type MultilingualString = {
-    [Property in Locale]: string
-}
