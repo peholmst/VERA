04c1e3e577f0c44341e97158c485357f89a3b101
Petter Holmström <petter.holmstrom@outlook.com>
2025-11-11 07:13:54 +0200

    Paint a minimal analog clock

--- cpp_modules/vera-station-alert/src/AnalogClock.cpp
+++ cpp_modules/vera-station-alert/src/AnalogClock.cpp
@@ -1,42 +1,89 @@
 #include <SDL3_gfxPrimitives.h>
 
 #include <algorithm>
+#include <chrono>
+#include <cmath>
 
 #include "AnalogClock.hpp"
-const uint32_t CLOCK_FACE_COLOR = RGBA(255, 255, 255);
+
+const uint32_t CLOCK_FACE_COLOR = RGBA(24, 25, 27);
 const uint32_t CLOCK_BORDER_COLOR = RGBA(54, 69, 79);
+const uint32_t CLOCK_HOUR_HAND_COLOR = RGBA(255, 255, 255);
+const uint32_t CLOCK_MINUTE_HAND_COLOR = RGBA(255, 255, 255);
+const uint32_t CLOCK_SECOND_HAND_COLOR = RGBA(231, 24, 11);
 
-static void PaintClockFace(SDL_Renderer *renderer, const float centerX, const float centerY, const float radius)
+static SDL_FPoint CalculatePointFromCenter(const SDL_FPoint &center, const float radius, const float angleRadians)
 {
-    // Clear
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    // Border    
-    filledCircleColor(renderer, centerX, centerY, radius, CLOCK_BORDER_COLOR);
-
-    // Face
-    filledCircleColor(renderer, centerX, centerY, radius * 0.95, CLOCK_FACE_COLOR);
-
-    // TODO Ticks
+    auto xOffset = radius * sin(angleRadians);
+    auto yOffset = radius * cos(angleRadians);
+    return SDL_FPoint{(float)(center.x + xOffset),
+                      (float)(center.y - yOffset)};
 }
 
-static void PaintHands(SDL_Renderer *renderer, const float centerX, const float centerY, const float radius, const std::chrono::time_point<std::chrono::system_clock> &time)
+static void PaintClockFace(SDL_Renderer *renderer, const SDL_FPoint &center, const float radius)
 {
-    // TODO Paint the hands
+    // Clear
+    SDL_SetRenderDrawColor(renderer, 36, 37, 39, 255);
+    SDL_RenderClear(renderer);
+
+    // Face
+    filledCircleColor(renderer, center.x, center.y, radius, CLOCK_FACE_COLOR);
+
+    /*    // Face
+        filledCircleColor(renderer, centerX, centerY, radius * 0.95, CLOCK_FACE_COLOR);*/
+
+    // TODO Ticks
+
+    // TODO Numbers
+}
+
+static void PaintHand(SDL_Renderer *renderer, const SDL_FPoint &center, const float radius, const float angleRadians, const float thickness, const uint32_t color)
+{
+    auto endPoint = CalculatePointFromCenter(center, radius, angleRadians);
+    thickLineColor(renderer, center.x, center.y, endPoint.x, endPoint.y, thickness, color);
+}
+
+static void PaintHands(SDL_Renderer *renderer, const SDL_FPoint &center, const float radius, const std::chrono::hh_mm_ss<std::chrono::system_clock::duration> &time)
+{
+    // TODO Use seconds instead and calculate the hours, minutes and seconds from it
+    auto hours = time.hours().count() % 12;
+    auto minutes = time.minutes().count();
+    auto seconds = time.seconds().count();
+
+    // Hour hand
+    auto hourRadians = ((hours * 60 + minutes) / 720.0f) * M_PI * 2;
+    PaintHand(renderer, center, radius * 0.5, hourRadians, radius * 0.03, CLOCK_HOUR_HAND_COLOR);
+
+    // Minute hand
+    auto minuteRadians = ((minutes + seconds / 60.0f) / 60.0f) * M_PI * 2;
+    PaintHand(renderer, center, radius * 0.65, minuteRadians, radius * 0.02, CLOCK_MINUTE_HAND_COLOR);
+
+    // Second hand
+    auto secondRadians = (seconds / 60.0f) * M_PI * 2;
+    PaintHand(renderer, center, radius * 0.7, secondRadians, radius * 0.01, CLOCK_SECOND_HAND_COLOR);
+
+    // Center
+    filledCircleColor(renderer, center.x, center.y, radius * 0.04, CLOCK_SECOND_HAND_COLOR);
+    filledCircleColor(renderer, center.x, center.y, radius * 0.02, CLOCK_MINUTE_HAND_COLOR);
 }
 
 AnalogClock::AnalogClock(const RendererPtr &renderer) : renderer(renderer)
 {
     auto size = GetRendererSize(renderer);
-    centerX = size.width / 2;
-    centerY = size.height / 2;
+    center = {size.width / 2.0f, size.height / 2.0f};
     radius = std::min(size.height, size.width) / 2 - 20;
 }
 
 void AnalogClock::Paint()
 {
-    PaintClockFace(renderer.get(), centerX, centerY, radius);
-    PaintHands(renderer.get(), centerX, centerY, radius, std::chrono::system_clock::now());
+    using namespace std::chrono;
+    auto now = system_clock::now();
+    auto local = zoned_time{current_zone(), now}.get_local_time();
+    auto today = floor<days>(local);
+    auto timeSinceMidnight = local - today;
+    hh_mm_ss hms{timeSinceMidnight};
+    
+    PaintClockFace(renderer.get(), center, radius);
+    PaintHands(renderer.get(), center, radius, hms);
     SDL_RenderPresent(renderer.get());
 }
--- cpp_modules/vera-station-alert/src/AnalogClock.hpp
+++ cpp_modules/vera-station-alert/src/AnalogClock.hpp
@@ -1,7 +1,6 @@
 #ifndef __ANALOG_CLOCK_HPP__
 #define __ANALOG_CLOCK_HPP__
 
-#include <chrono>
 #include <SDL3/SDL.h>
 
 #include "SDLHelpers.hpp"
@@ -13,7 +12,8 @@ public:
     void Paint();
 private:
     const RendererPtr &renderer;
-    float radius, centerX, centerY;
+    float radius;
+    SDL_FPoint center;
 };
 
 #endif
