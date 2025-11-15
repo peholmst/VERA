cb048bea3f4215be30cd02d17cc5611e144eb4a3
Petter Holmström <petter.holmstrom@outlook.com>
2025-11-15 13:29:56 +0200

    Add more fonts

--- cpp_modules/vera-station-alert/src/FontHelpers.hpp
+++ cpp_modules/vera-station-alert/src/FontHelpers.hpp
@@ -4,14 +4,43 @@
 #include "Fonts.hpp"
 #include "SDLHelpers.hpp"
 
-FontPtr CreateRobotoRegular(const float &pointSize)
+FontPtr CreateFont(const void *data, const size_t dataSize, const float &pointSize)
 {
     FontPtr font{
-        TTF_OpenFontIO(SDL_IOFromConstMem(Fonts::Roboto_Regular_ttf, Fonts::Roboto_Regular_ttf_size), true, pointSize),
-        SDLDeleter{}  
-    };
+        TTF_OpenFontIO(SDL_IOFromConstMem(data, dataSize), true, pointSize),
+        SDLDeleter{}};
     SDLCheck(font != nullptr, "SDL_OpenFontIO");
     return font;
+}
+
+FontPtr CreateRobotoBlack(const float &pointSize)
+{
+    return CreateFont(Fonts::Roboto_Black_ttf, Fonts::Roboto_Black_ttf_size, pointSize);
+}
+
+FontPtr CreateRobotoBold(const float &pointSize)
+{
+    return CreateFont(Fonts::Roboto_Bold_ttf, Fonts::Roboto_Bold_ttf_size, pointSize);
+}
+
+FontPtr CreateRobotoExtraBold(const float &pointSize)
+{
+    return CreateFont(Fonts::Roboto_ExtraBold_ttf, Fonts::Roboto_ExtraBold_ttf_size, pointSize);
+}
+
+FontPtr CreateRobotoMedium(const float &pointSize)
+{
+    return CreateFont(Fonts::Roboto_Medium_ttf, Fonts::Roboto_Medium_ttf_size, pointSize);
+}
+
+FontPtr CreateRobotoRegular(const float &pointSize)
+{
+    return CreateFont(Fonts::Roboto_Regular_ttf, Fonts::Roboto_Regular_ttf_size, pointSize);
+}
+
+FontPtr CreateRobotoSemiBold(const float &pointSize)
+{
+    return CreateFont(Fonts::Roboto_SemiBold_ttf, Fonts::Roboto_SemiBold_ttf_size, pointSize);
 }
 
 #endif
--- cpp_modules/vera-station-alert/src/Fonts.hpp
+++ cpp_modules/vera-station-alert/src/Fonts.hpp
@@ -5,10 +5,25 @@
 
 namespace Fonts
 {
+    // TODO Delete unused fonts
+
+    extern unsigned char Roboto_Black_ttf[];
+    extern size_t Roboto_Black_ttf_size;
+
+    extern unsigned char Roboto_Bold_ttf[];
+    extern size_t Roboto_Bold_ttf_size;
+
+    extern unsigned char Roboto_ExtraBold_ttf[];
+    extern size_t Roboto_ExtraBold_ttf_size;
+
+    extern unsigned char Roboto_Medium_ttf[];
+    extern size_t Roboto_Medium_ttf_size;
 
     extern unsigned char Roboto_Regular_ttf[];
     extern size_t Roboto_Regular_ttf_size;
-
+    
+    extern unsigned char Roboto_SemiBold_ttf[];
+    extern size_t Roboto_SemiBold_ttf_size;
 };
 
 #endif
